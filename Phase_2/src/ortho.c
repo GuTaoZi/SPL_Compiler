fd1e51f91cbc5725ff4f535def01de0a70cc90d2
GuTaoZi <gutao.official@gmail.com>
2023-11-24 12:44:31 +0800

    ortho add done

--- Phase_2/src/ortho.c
+++ Phase_2/src/ortho.c
@@ -1,12 +1,14 @@
 #include "type.h"
 #include "uthash.h"
+#include <stdio.h>
+#include <string.h>
 
 typedef struct _orthoNode
 {
     char name[32];
     orthoNode *next[2];
-    // next[0]: ptr to next node with same name
-    // next[1]: ptr to next node within same scope
+    // next[0]: ptr to next node with same name(hash)
+    // next[1]: ptr to next node within same scope(stack)
     // NULL for tail of the list
     Type *val;
 } orthoNode;
@@ -16,15 +18,19 @@ typedef struct _hashNode
     char name[32];
     orthoNode *head;
     UT_hash_handle hh;
-}hashNode;
+} hashNode;
+
+static hashNode *H = NULL;
 
 typedef struct _orthoStack
 {
-    orthoNode *head;
+    orthoNode *top;
     orthoStack *next;
 } orthoStack;
 
-orthoNode *new_node(char *name, Type *val)
+static orthoStack *S = NULL;
+
+static orthoNode *new_ortho_node(char *name, Type *val)
 {
     orthoNode *p = (orthoNode *)malloc(sizeof(orthoNode));
     p->name = name;
@@ -33,7 +39,67 @@ orthoNode *new_node(char *name, Type *val)
     return p;
 }
 
-orthoNode *get_head_by_name(char *name, )
+orthoStack *new_stack_node()
 {
+    orthoStack *p = (orthoStack *)malloc(sizeof(orthoStack));
+    p->top = NULL;
+    p->next = S;
+    S = p;
+    return S;
+}
 
+orthoNode *add_ortho_node(char *name, Type *val)
+{
+    if (!S)
+    {
+        fprintf("NULL stack get, need to new a stack node.\n");
+        return NULL;
+    }
+    orthoNode *p = new_ortho_node(name, val);
+    hashNode *hn = (hashNode *)malloc(sizeof(hashNode));
+
+    hashNode *old_hash_head;
+    HASH_FIND_STR(H, name, old_hash_head);
+    if (old_hash_head)
+    {
+        p->next[0] = old_hash_head->head;
+        // place the new head ahead of the old head
+        HASH_DEL(H, old_hash_head);
+        // delete the old map <name, hashNode(containing orthoNode)>
+    }
+    if (S)
+    {
+        p->next[1] = S->top;
+        S->top = p;
+    }
+    strcpy(hn->name, name);
+    hn->head = p;
+    HASH_ADD(H, name, hn);
+    // add the new map <name, hashNode(containing orthoNode)>
+}
+
+orthoStack *pop_stack()
+{
+    orthoNode *p = S->top;
+    if (!p)
+    {
+        fprintf("Trying to del a NULL node.\n");
+        return NULL;
+    }
+}
+
+orthoNode *get_head_by_name(char *name)
+{
+    hashNode *p;
+    HASH_FIND_STR(H, name, p);
+    if (p)
+    {
+        return p->head;
+    }
+    return NULL;
+}
+
+orthoNode *stack_top()
+{
+    return S ? S->top : NULL;
 }
