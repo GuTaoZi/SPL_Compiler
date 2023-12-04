45d0800b6048017bb3c827c49e1bf0f2d4bee118
GuTaoZi <gutao.official@gmail.com>
2023-12-04 15:11:56 +0800

    update testcases from other students

--- Phase_2/test_others.sh
+++ Phase_2/test_others.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 for i in {01..13}; do
-    ./bin/splc ./test-others/test_1_s$i.spl
-    diff ./test-others/test_1_s$i.out ./test-others/out-std/test_1_s$i.out > /dev/null
+    ./bin/splc ./test-others/test_2_s$i.spl
+    diff ./test-others/test_2_s$i.out ./test-others/out-std/test_2_s$i.out > /dev/null
     if [ $? -ne 0 ]; then
         echo "Test $i failed!"
         # break
