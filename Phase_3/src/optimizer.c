374e713f6ea5bd70406c2239fb163462b53fba90
Artanisax <1415197174@qq.com>
2023-12-24 21:30:22 +0800

    remove debug info

--- Phase_3/src/optimizer.c
+++ Phase_3/src/optimizer.c
@@ -93,8 +93,8 @@ void split_str(const char *s)
 
 void del_list(IR_list *p)
 {
-fprintf(debug, "del: ");
-debug_IR_list(p, false);
+// fprintf(debug, "del: ");
+// debug_IR_list(p, false);
     if (p->prev != NULL)
     {
         p->prev->next = p->next;
@@ -378,12 +378,12 @@ bool find_identity(IR_list *ir) // x is not const
     // (*)x := (*&)y
     if (get_list_len(ir) == 3)
     {
-fprintf(debug, "len = 3\n");
+// fprintf(debug, "len = 3\n");
         Var *y = get_var(ir->ss[2]);
         if (not_tmp(y) || ir->ss[2][0] == '&')
             return optimized;
         // (*)x := *y
-fprintf(debug, "y is singel? %d\n", single_parent(y));
+// fprintf(debug, "y is singel? %d\n", single_parent(y));
         if (ir->ss[2][0] == '*')
         {
             if (!single_parent(y) || y->parent[0].usage != VAL)
@@ -406,7 +406,7 @@ fprintf(debug, "y is singel? %d\n", single_parent(y));
                 return optimized;
             if (single_parent(y))
             {
-fprintf(debug, "single, (&)y\n");
+// fprintf(debug, "single, (&)y\n");
                 Parent py = y->parent[0];
                 if (py.var == NULL || py.recent != py.var->recent)
                     return optimized;
@@ -443,12 +443,12 @@ fprintf(debug, "single, (&)y\n");
     // (*)x := (*&)y ? (*&)z
     else
     {
-fprintf(debug, "(*)x := (*&)y ? (*&)z\n");
+// fprintf(debug, "(*)x := (*&)y ? (*&)z\n");
         Var *y = get_var(ir->ss[2]), *z = get_var(ir->ss[4]);
         // optimize (*)y
         if (!not_tmp(y) && ir->ss[2][0] != '&' && single_parent(y))
         {
-fprintf(debug, "y is single\n");
+// fprintf(debug, "y is single\n");
             // *y
             if (ir->ss[2][0] == '*')
             {
@@ -468,7 +468,7 @@ fprintf(debug, "y is single\n");
             // y
             else
             {
-fprintf(debug, "y is not a PTR\n");
+// fprintf(debug, "y is not a PTR\n");
                 if (y->type == CONST)
                 {
                     free(ir->ss[2]);
@@ -480,10 +480,10 @@ fprintf(debug, "y is not a PTR\n");
                 else
                 {
                     Parent py = y->parent[0];
-fprintf(debug, "y is not a CONST\n");
+// fprintf(debug, "y is not a CONST\n");
                     if (py.recent == py.var->recent)
                     {
-fprintf(debug, "py: %d %s\n", py.usage, get_name(py.var));
+// fprintf(debug, "py: %d %s\n", py.usage, get_name(py.var));
                         free(ir->ss[2]);
                         ir->ss[2] = prefixed_name(py.usage, get_name(py.var));
                         if (usage != PTR)
@@ -579,8 +579,8 @@ bool simplify_assign(IR_list *ir)
         x->recent = ir;
         x->type = VAR;
     }
-fprintf(debug, "in sa(): %d\n", get_list_len(ir));
-debug_IR_list(ir, false);
+// fprintf(debug, "in sa(): %d\n", get_list_len(ir));
+// debug_IR_list(ir, false);
     switch (get_list_len(ir))
     {
         // (*)x := (*&)y
@@ -609,7 +609,7 @@ debug_IR_list(ir, false);
             char op = ir->ss[3][0];
             x->parent[0] = (Parent){get_usage(ir->ss[2]), y, y->recent};
             x->parent[1] = (Parent){get_usage(ir->ss[4]), z, z->recent};
-fprintf(debug, "in sa(): case 5: %d %s\n", x->parent[0].usage, ir->ss[2]);
+// fprintf(debug, "in sa(): case 5: %d %s\n", x->parent[0].usage, ir->ss[2]);
             // #? 
             if (!not_tmp(y) && !not_tmp(z) && y->type == CONST && z->type == CONST)
             {
@@ -648,7 +648,7 @@ fprintf(debug, "in sa(): case 5: %d %s\n", x->parent[0].usage, ir->ss[2]);
                 ((op == '+' && z->type == CONST && z->val == 0) ||
                 (op == '-' && z->type == CONST && z->val == 0)))
             {
-fprintf(debug, "+- 0\n");
+// fprintf(debug, "+- 0\n");
                 free(ir->ss[3]);
                 free(ir->ss[4]);
                 ir->ss[3] = ir->ss[4] = NULL;
@@ -668,10 +668,10 @@ fprintf(debug, "+- 0\n");
             }
             break;
     }
-debug_IR_list(ir, false);
+// debug_IR_list(ir, false);
     if (x->type != CONST)
         optimized |= find_identity(ir);
-debug_IR_list(ir, true);
+// debug_IR_list(ir, true);
     return optimized;
 }
 
@@ -936,7 +936,7 @@ int main(int argc, char **argv)
     FILE *fin = fopen(argv[1], "r");
     FILE *fout = fopen(argv[2], "w");
     // fprintf(debug, "%s %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4], argv[5]);
-debug = fopen("./test-tmp/debug_info.txt", "w");
+// debug = fopen("./test-tmp/debug_info.txt", "w");
     if (fin == NULL || fout == NULL)
     {
         fprintf(stderr, "Open file error\n");
